e8d90f0ef28295c8f87f471f41845345289231bb
Quentin Lambotte <quentin.lambotte@hotmail.com>
2022-01-26 14:51:14 +0100

    fix mistake

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         hang = game.Hangman()
         hang.start_game()
-    elif len(sys.argv) > 1:
+    elif len(sys.argv) == 2:
         arg = sys.argv[1]
         if arg.isdigit():
             hang = game.Hangman(lives=int(arg))
@@ -16,7 +16,7 @@ if __name__ == "__main__":
             hang = game.Hangman(possible_words=possible_words)
             hang.start_game()
     else:
-        with open(arg, "r") as file:
+        with open(sys.argv[2], "r") as file:
             possible_words = file.read().split("\n")
         hang = game.Hangman(
             lives=int(sys.argv[1]), possible_words=possible_words
